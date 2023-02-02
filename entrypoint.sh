d25d25f97b81db4ac6d594b219031500c88278b5
Eyal Halfon <eyalh@nimbleway.com>
2023-02-02 14:35:00 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+echo "env: $ENVIRONMENT"
+echo "branch: $BRANCH"
 
-if [ $ENVIRONMENT = 'beta' && $BRANCH = 'master' || $BRANCH = 'main' ]; then
+if [[ $ENVIRONMENT == 'beta' && ($BRANCH != 'master' || $BRANCH != 'main') ]]; then
+  echo "false"
   echo "result=false" >> $GITHUB_OUTPUT
 else
+  echo "true"
   echo "result=true" >> $GITHUB_OUTPUT
 fi
 
